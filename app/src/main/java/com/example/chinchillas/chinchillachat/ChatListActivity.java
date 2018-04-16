a12b5286edce9897d7b578f6f3ba5cbc0c3d0d18
Vecna <ivyvecna15@augustana.edu>
2018-04-16 16:19:07 -0500

    Working towards messages...

--- app/src/main/java/com/example/chinchillas/chinchillachat/ChatListActivity.java
+++ app/src/main/java/com/example/chinchillas/chinchillachat/ChatListActivity.java
@@ -1,8 +1,38 @@
 package com.example.chinchillas.chinchillachat;
 
-/**
- * Created by ivyvecna15 on 4/16/2018.
- */
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ChatListActivity extends ChinchillaChatActivity {
+    private Button newChatBtn;
+    private ScrollView scrollView;
+
+    private FirebaseAuth firebaseAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        firebaseAuth = FirebaseAuth.getInstance();
+        setContentView(R.layout.activity_chatlist);
+
+        newChatBtn = findViewById(R.id.newChatBtn);
+        newChatBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), NewChatActivity.class));
+            }
+        });
+
+
+        scrollView = findViewById(R.id.scrollChats);
+        // TODO: ADD CODE TO FILL SCROLLVIEW WITH CURRENT CHATS
+
+    }
+
 }
